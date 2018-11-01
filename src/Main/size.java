a4477bcb371e8218a70604d610b18abb9cbbfddb
galea <galea@Rafael-NASA>
2018-10-31 23:21:32 -0500

    Working buttons

--- src/Main/size.java
+++ src/Main/size.java
@@ -5,6 +5,8 @@
  */
 package Main;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author ragaleano
@@ -34,10 +36,10 @@ public class size extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(315, 150));
-        setMinimumSize(new java.awt.Dimension(315, 150));
+        setMaximumSize(new java.awt.Dimension(316, 120));
+        setMinimumSize(new java.awt.Dimension(316, 120));
         setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(315, 150));
+        setPreferredSize(new java.awt.Dimension(316, 120));
         setResizable(false);
         getContentPane().setLayout(null);
 
@@ -50,18 +52,60 @@ public class size extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Buttons/15Xbase.png"))); // NOI18N
         jButton1.setContentAreaFilled(false);
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jButton1MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jButton1MouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jButton1MousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton1MouseReleased(evt);
+            }
+        });
         getContentPane().add(jButton1);
         jButton1.setBounds(10, 60, 90, 40);
 
         jButton2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Buttons/19Xbase.png"))); // NOI18N
         jButton2.setContentAreaFilled(false);
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jButton2MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jButton2MouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jButton2MousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton2MouseReleased(evt);
+            }
+        });
         getContentPane().add(jButton2);
         jButton2.setBounds(110, 60, 90, 40);
 
         jButton3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Buttons/21Xbase.png"))); // NOI18N
         jButton3.setContentAreaFilled(false);
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jButton3MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jButton3MouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jButton3MousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton3MouseReleased(evt);
+            }
+        });
         getContentPane().add(jButton3);
         jButton3.setBounds(210, 60, 90, 40);
 
@@ -72,6 +116,66 @@ public class size extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseEntered
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/15Xtilted.png"));
+        jButton1.setIcon(ii);
+    }//GEN-LAST:event_jButton1MouseEntered
+
+    private void jButton1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseExited
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/15Xbase.png"));
+        jButton1.setIcon(ii);
+    }//GEN-LAST:event_jButton1MouseExited
+
+    private void jButton1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MousePressed
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/15Xpressed.png"));
+        jButton1.setIcon(ii);
+    }//GEN-LAST:event_jButton1MousePressed
+
+    private void jButton1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseReleased
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/15Xbase.png"));
+        jButton1.setIcon(ii);
+    }//GEN-LAST:event_jButton1MouseReleased
+
+    private void jButton2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseEntered
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/19Xtilted.png"));
+        jButton2.setIcon(ii);
+    }//GEN-LAST:event_jButton2MouseEntered
+
+    private void jButton2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseExited
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/19Xbase.png"));
+        jButton2.setIcon(ii);
+    }//GEN-LAST:event_jButton2MouseExited
+
+    private void jButton2MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MousePressed
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/19Xpressed.png"));
+        jButton2.setIcon(ii);
+    }//GEN-LAST:event_jButton2MousePressed
+
+    private void jButton2MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseReleased
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/19Xbase.png"));
+        jButton2.setIcon(ii);
+    }//GEN-LAST:event_jButton2MouseReleased
+
+    private void jButton3MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseEntered
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/21Xtilted.png"));
+        jButton3.setIcon(ii);
+    }//GEN-LAST:event_jButton3MouseEntered
+
+    private void jButton3MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseExited
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/21Xbase.png"));
+        jButton3.setIcon(ii);
+    }//GEN-LAST:event_jButton3MouseExited
+
+    private void jButton3MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MousePressed
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/21Xpressed.png"));
+        jButton3.setIcon(ii);
+    }//GEN-LAST:event_jButton3MousePressed
+
+    private void jButton3MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseReleased
+        ImageIcon ii = new ImageIcon(getClass().getResource("/Buttons/21Xbase.png"));
+        jButton3.setIcon(ii);
+    }//GEN-LAST:event_jButton3MouseReleased
 
     /**
      * @param args the command line arguments
